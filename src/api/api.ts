05aee046d66cf670c84921521311f041452c79de
astromona <gest@MacBook-Pro-Gost.local>
2023-06-10 22:43:04 +0300

    change requests config

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,11 +2,6 @@ import axios from 'axios'
 
 const $api = axios.create({
   baseURL: '/api',
-  proxy: {
-    host: '45.12.75.188',
-    port: 5000,
-    protocol: 'http'
-  }
 })
 
 export default $api
